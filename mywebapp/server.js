72832df18ffafdbcdb4cea9ebe09111b568b220e
Liza-Khube <elisekhu@gmail.com>
2026-05-01 16:08:31 +0300

    Update server to listen on systemd socket

--- mywebapp/server.js
+++ mywebapp/server.js
@@ -4,6 +4,12 @@ const config = require('./config');
 const host = config.server.host;
 const port = config.server.port;
 
-app.listen(port, host, () => {
-  console.log(`mywebapp listening on ${host}: ${port}`);
-});
+if (process.env.LISTEN_FDS && parseInt(process.env.LISTEN_FDS) >= 1) {
+  app.listen({ fd: 3 }, () => {
+    console.log('mywebapp listening on systemd socket');
+  });
+} else {
+  app.listen(port, host, () => {
+    console.log(`mywebapp listening on ${host}: ${port}`);
+  });
+}
